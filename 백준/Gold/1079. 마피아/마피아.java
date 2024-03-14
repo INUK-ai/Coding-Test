1b5ff6a1b1274f1a608c7f1e6e222ccb72fd59cd
INUK-ai <64780539+INUK-ai@users.noreply.github.com>
2024-03-14 16:15:27 +0900

    [Gold II] Title: 마피아, Time: 600 ms, Memory: 24200 KB -BaekjoonHub

--- 백준/Gold/1079. 마피아/마피아.java
+++ 백준/Gold/1079. 마피아/마피아.java
@@ -9,7 +9,7 @@ public class Main {
     static int N, Eunjin, answer = 0;
     static int[] guilty;
     static int[][] murder;
-    static boolean[] alive;
+    static boolean[] dead;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -18,9 +18,7 @@ public class Main {
         N = Integer.parseInt(bf.readLine());
         guilty = new int[N];
         murder = new int[N][N];
-        alive = new boolean[N];
-
-        Arrays.fill(alive, true);
+        dead = new boolean[N];
 
         st = new StringTokenizer(bf.readLine());
         for(int i = 0; i < N; i++) {
@@ -46,31 +44,31 @@ public class Main {
 
     private static void play(int num, int day) {
 
-        if(!alive[Eunjin] || num == 1) {
+        if(dead[Eunjin] || num == 1) {
             answer = Math.max(answer, day);
             return;
         }
 
         if(num % 2 == 0) {
             for(int i = 0; i < N; i++) {
-                if(!alive[i] || i == Eunjin) {
+                if(dead[i] || i == Eunjin) {
                     continue;
                 }
 
                 for(int j = 0; j < N; j++) {
-                    if(!alive[j]) {
+                    if(dead[j]) {
                         continue;
                     }
 
                     guilty[j] += murder[i][j];
                 }
 
-                alive[i] = false;
+                dead[i] = true;
                 play(num - 1, day + 1);
-                alive[i] = true;
+                dead[i] = false;
 
                 for(int j = 0; j < N; j++) {
-                    if(!alive[j]) {
+                    if(dead[j]) {
                         continue;
                     }
 
@@ -79,21 +77,18 @@ public class Main {
             }
         } else {
             int max = 0;
-            int idx = N - 1;
+            int idx = 0;
 
-            for(int i = 0; i < N; i++) {
-                if(alive[i] && max < guilty[i]) {
+            for(int i = N - 1; i >= 0; i--) {
+                if(!dead[i] && max <= guilty[i]) {
                     max = guilty[i];
                     idx = i;
-                } else if(alive[i] && max == guilty[i]) {
-                    max = guilty[i];
-                    idx = Math.min(idx, i);
                 }
             }
 
-            alive[idx] = false;
+            dead[idx] = true;
             play(num - 1, day);
-            alive[idx] = true;
+            dead[idx] = false;
         }
     }
 }
