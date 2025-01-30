c16dcc40d74aa7178922211d9cd0d1858226cd88
INUK-ai <64780539+INUK-ai@users.noreply.github.com>
2025-01-30 21:21:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.18 ms, Memory: 89.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        boolean isUpper = true;
+
+        for (char c : s.toCharArray()) {
+            if (!Character.isAlphabetic(c)) {
+                sb.append(c);
+                isUpper = true;
+                continue;
+            }
+
+            if (isUpper) {
+                sb.append(Character.toUpperCase(c));
+            } else {
+                sb.append(Character.toLowerCase(c));
+            }
+            
+            isUpper = !isUpper;
+        }
+
+        return sb.toString();
+    }
+}
