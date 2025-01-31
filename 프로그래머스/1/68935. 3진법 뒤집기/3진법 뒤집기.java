121de810422338f48706bfd59ef98fae5dfab8e2
INUK-ai <64780539+INUK-ai@users.noreply.github.com>
2025-01-31 21:10:29 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,10 @@
+class Solution {
+
+    public int solution(int n) {
+
+        String string_n = Integer.toString(n, 3);
+        String reverse_str = new StringBuilder(string_n).reverse().toString();
+
+        return Integer.valueOf(reverse_str, 3);
+    }
+}
