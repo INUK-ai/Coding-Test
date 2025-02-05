e218a6307c61b453cdd491a677a7f52b838ef4aa
INUK-ai <64780539+INUK-ai@users.noreply.github.com>
2025-02-06 01:08:04 +0900

    [level 2] Title: 가장 큰 수, Time: 210.06 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,30 +1,18 @@
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class Solution {
+
     public String solution(int[] numbers) {
-        
-        String[] strs = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            strs[i] = String.valueOf(numbers[i]);
-        }
-        
-        Arrays.sort(strs, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return (o2 + o1).compareTo(o1 + o2);
-            }
-        });
-        
-        if (strs[0].equals("0")) {
-            return "0";
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        for (String str : strs) {
-            sb.append(str);
-        }
-        
-        return sb.toString();
+
+        return Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .sorted((s1, s2) -> {
+                    int original = Integer.parseInt(s1 + s2);
+                    int reversed = Integer.parseInt(s2 + s1);
+                    return reversed - original;
+                })
+                .collect(Collectors.joining(""))
+                .replaceAll("^0+", "0");
     }
 }
