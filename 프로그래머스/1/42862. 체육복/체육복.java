8b6c0b61a2aea6850cecbf7296d98ee773665c92
INUK-ai <64780539+INUK-ai@users.noreply.github.com>
2025-02-08 07:33:14 +0900

    [level 1] Title: 체육복, Time: 4.76 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+
+    public int solution(int n, int[] lost, int[] reserve) {
+
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        Set<Integer> owns = Arrays.stream(lost)
+                .boxed()
+                .collect(Collectors.toSet());
+
+        owns.retainAll(Arrays.stream(reserve)
+                .boxed()
+                .collect(Collectors.toSet()));
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int l : lost) queue.add(l);
+
+        int get = 0;
+        for (int r : reserve) {
+            if (owns.contains(r)) continue;
+
+            while (!queue.isEmpty() && (queue.peek() < r - 1 || owns.contains(queue.peek()))) {
+                queue.poll();
+            }
+
+            if (queue.isEmpty()) break;
+
+            if (queue.peek() <= r + 1) {
+                queue.poll();
+                get++;
+            }
+        }
+
+        return n - lost.length + owns.size() + get;
+    }
+}
