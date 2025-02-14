f5e42c59e50b66c5332e94e4a402a078ec49b1c5
INUK-ai <64780539+INUK-ai@users.noreply.github.com>
2025-02-14 11:09:02 +0900

    [Silver III] Title: N과 M (1), Time: 244 ms, Memory: 22428 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    private static int N, M;
+    private static int[] sequence;
+    private static boolean[] visited;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        sequence = new int[M];
+        visited = new boolean[N + 1];
+        
+        backtrace(0);
+
+        System.out.println(sb);
+    }
+    
+    private static void backtrace(int depth) {
+        
+        if (depth == M) {
+            for (int num : sequence) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                sequence[depth] = i;
+                backtrace(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
