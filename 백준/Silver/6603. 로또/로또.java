5b6bc0370570081234d6b0d199c3328b9eaf726a
INUK-ai <64780539+INUK-ai@users.noreply.github.com>
2025-02-16 15:28:00 +0900

    [Silver II] Title: 로또, Time: 120 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int k;
+    private static int[] nums;
+    private static int[] lotto;
+    private static boolean[] visited;
+
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            k = Integer.parseInt(st.nextToken());
+
+            if (k == 0) break;
+
+            nums = new int[k];
+            lotto = new int[6];
+            visited = new boolean[k];
+
+            for (int i = 0; i < k; i++) {
+                nums[i] = Integer.parseInt(st.nextToken());
+            }
+
+            backtrace(0, 0);
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private static void backtrace(int index, int count) {
+
+        if (count == 6) {
+            for (int lottoNum : lotto) {
+                sb.append(lottoNum).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                lotto[count] = nums[i];
+                backtrace(i, count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
