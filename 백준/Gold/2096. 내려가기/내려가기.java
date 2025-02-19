100b38d9831cff989eea365127d16e062b54b89e
INUK-ai <64780539+INUK-ai@users.noreply.github.com>
2025-02-19 11:48:36 +0900

    [Gold V] Title: 내려가기, Time: 356 ms, Memory: 53748 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -10,7 +10,7 @@ public class Main {
     private static StringBuilder sb = new StringBuilder();
 
     private static int N;
-    private static int[][] map, minDp, maxDp, tempMax, tempMin;
+    private static int[][] map, minDp, maxDp;
 
     private static int[] dx = {-1, 0, 1};
     private static int[] dy = {1, 1, 1};
@@ -23,8 +23,6 @@ public class Main {
         map = new int[N][3];
         minDp = new int[N][3];
         maxDp = new int[N][3];
-        tempMax = new int[N][3];
-        tempMin = new int[N][3];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 3; i++) {
@@ -38,18 +36,13 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
 
-            tempMax[i][0] = Math.max(maxDp[i - 1][0], maxDp[i - 1][1]) + map[i][0];
-            tempMax[i][1] = Math.max(maxDp[i - 1][0], Math.max(maxDp[i - 1][1], maxDp[i - 1][2])) + map[i][1];
-            tempMax[i][2] = Math.max(maxDp[i - 1][1], maxDp[i - 1][2]) + map[i][2];
+            maxDp[i][0] = Math.max(maxDp[i - 1][0], maxDp[i - 1][1]) + map[i][0];
+            maxDp[i][1] = Math.max(maxDp[i - 1][0], Math.max(maxDp[i - 1][1], maxDp[i - 1][2])) + map[i][1];
+            maxDp[i][2] = Math.max(maxDp[i - 1][1], maxDp[i - 1][2]) + map[i][2];
 
-            tempMin[i][0] = Math.min(minDp[i - 1][0], minDp[i - 1][1]) + map[i][0];
-            tempMin[i][1] = Math.min(minDp[i - 1][0], Math.min(minDp[i - 1][1], minDp[i - 1][2])) + map[i][1];
-            tempMin[i][2] = Math.min(minDp[i - 1][1], minDp[i - 1][2]) + map[i][2];
-
-            for (int j = 0; j < 3; j++) {
-                maxDp[i][j] = tempMax[i][j];
-                minDp[i][j] = tempMin[i][j];
-            }
+            minDp[i][0] = Math.min(minDp[i - 1][0], minDp[i - 1][1]) + map[i][0];
+            minDp[i][1] = Math.min(minDp[i - 1][0], Math.min(minDp[i - 1][1], minDp[i - 1][2])) + map[i][1];
+            minDp[i][2] = Math.min(minDp[i - 1][1], minDp[i - 1][2]) + map[i][2];
         }
 
         int maxScore = Math.max(maxDp[N - 1][0], Math.max(maxDp[N - 1][1], maxDp[N - 1][2]));
