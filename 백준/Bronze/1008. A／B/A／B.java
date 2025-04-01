e7fe0edab8558e30352402402afad7ae6631cf86
INUK-ai <64780539+INUK-ai@users.noreply.github.com>
2025-04-01 20:16:50 +0900

    [Bronze V] Title: A/B, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+
+    private static int N, M;
+    private static int[][] board;
+    private static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        double result = (double) N / M;
+
+        System.out.println(result);
+    }
+}
